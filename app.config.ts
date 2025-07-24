5bcd8d2d50501a2514d8201aa1f671011dfb09a1
Alekos Filini <alekos.filini@gmail.com>
2025-07-24 14:40:32 +0200

    Fix icon and colors (#65)
    (cherry picked from commit d203365cea7f60bd4a29b0edbf05ec86fa5cdde4)

--- app.config.ts
+++ app.config.ts
@@ -1,103 +1,98 @@
 export default {
-  "expo": {
-    "name": "Portal",
-    "slug": "portal",
-    "version": "1.0.0",
-    "orientation": "portrait",
-    "owner": "portaltechnologiesinc",
-    "icon": "./assets/images/appLogo.png",
-    "scheme": "portal",
-    "userInterfaceStyle": "dark",
-    "newArchEnabled": true,
-    "splash": {
-      "image": "./assets/images/appLogo.png",
-      "resizeMode": "contain",
-      "backgroundColor": "#000000"
+  expo: {
+    name: 'Portal',
+    slug: 'portal',
+    version: '1.0.0',
+    orientation: 'portrait',
+    owner: 'portaltechnologiesinc',
+    icon: './assets/images/appLogo.png',
+    scheme: 'portal',
+    userInterfaceStyle: 'dark',
+    newArchEnabled: true,
+    splash: {
+      image: './assets/images/appSplash.png',
+      resizeMode: 'contain',
+      backgroundColor: '#141416',
     },
-    "androidNavigationBar": {
-      "backgroundColor": "#000000"
+    androidNavigationBar: {
+      backgroundColor: '#141416',
     },
-    "ios": {
-      "supportsTablet": true,
-      "userInterfaceStyle": "dark",
-      "bundleIdentifier": "cc.getportal.portal",
-      "associatedDomains": [
-        "applinks:portal.app"
-      ],
-      "infoPlist": {
-        "NSCameraUsageDescription": "Portal uses your camera to scan for QR codes"
+    ios: {
+      supportsTablet: true,
+      userInterfaceStyle: 'dark',
+      bundleIdentifier: 'cc.getportal.portal',
+      associatedDomains: ['applinks:portal.app'],
+      infoPlist: {
+        NSCameraUsageDescription: 'Portal uses your camera to scan for QR codes',
       },
-      "icon": {
-        "dark": "./assets/images/iosDark.png",
-        "light": "./assets/images/iosLight.png",
-        "tinted": "./assets/images/iosTinted.png"
-      }
-    },
-    "android": {
-      "googleServicesFile": process.env.GOOGLE_SERVICES_JSON,
-      "adaptiveIcon": {
-        "foregroundImage": "./assets/images/appLogo.png",
-        "backgroundColor": "#000000"
+      icon: {
+        dark: './assets/images/iosDark.png',
+        light: './assets/images/iosLight.png',
+        tinted: './assets/images/iosTinted.png',
       },
-      "package": "cc.getportal.portal",
-      "userInterfaceStyle": "dark",
-      "intentFilters": [
+    },
+    android: {
+      googleServicesFile: process.env.GOOGLE_SERVICES_JSON,
+      adaptiveIcon: {
+        foregroundImage: './assets/images/appLogo.png',
+        backgroundColor: '#141416',
+      },
+      package: 'cc.getportal.portal',
+      userInterfaceStyle: 'dark',
+      intentFilters: [
         {
-          "action": "VIEW",
-          "autoVerify": true,
-          "data": [
+          action: 'VIEW',
+          autoVerify: true,
+          data: [
             {
-              "scheme": "portal"
-            }
+              scheme: 'portal',
+            },
           ],
-          "category": [
-            "BROWSABLE",
-            "DEFAULT"
-          ]
-        }
-      ]
-    },
-    "web": {
-      "bundler": "metro",
-      "output": "static",
-      "favicon": "./assets/images/favicon.png"
-    },
-    "plugins": [
-      "expo-router",
-      [
-        "expo-splash-screen",
-        {
-          "image": "./assets/images/appLogo.png",
-          "imageWidth": 200,
-          "resizeMode": "contain",
-          "backgroundColor": "#000000"
-        }
+          category: ['BROWSABLE', 'DEFAULT'],
+        },
       ],
-      "expo-secure-store",
-      "expo-sqlite",
-      "expo-web-browser",
+    },
+    web: {
+      bundler: 'metro',
+      output: 'static',
+      favicon: './assets/images/favicon.png',
+    },
+    plugins: [
+      'expo-router',
       [
-        "expo-notifications",
+        'expo-splash-screen',
         {
-          "icon": "./local/assets/appNotificationLogo.png",
-          "color": "#ffffff",
-          "defaultChannel": "default",
+          image: './assets/images/appSplash.png',
+          imageWidth: 200,
+          resizeMode: 'contain',
+          backgroundColor: '#141416',
+        },
+      ],
+      'expo-secure-store',
+      'expo-sqlite',
+      'expo-web-browser',
+      [
+        'expo-notifications',
+        {
+          icon: './assets/images/appNotificationLogo.png',
+          color: '#ffffff',
+          defaultChannel: 'default',
           // "sounds": [
           //   "./local/assets/notification_sound.wav",
           //   "./local/assets/notification_sound_other.wav"
           // ],
-          "enableBackgroundRemoteNotifications": true
-        }
-      ]
+          enableBackgroundRemoteNotifications: true,
+        },
+      ],
     ],
-    "experiments": {
-      "typedRoutes": true
+    experiments: {
+      typedRoutes: true,
     },
-    "extra": {
-      "router": {},
-      "eas": {
-        "projectId": "8aa33e4a-b2db-43ab-832b-709fb7f2ec0d"
-      }
-    }
-  }
+    extra: {
+      router: {},
+      eas: {
+        projectId: '8aa33e4a-b2db-43ab-832b-709fb7f2ec0d',
+      },
+    },
+  },
 };
